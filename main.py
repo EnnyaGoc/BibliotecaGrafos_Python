48553793979401ddb6bda831263c87a13b94e707
Isabele Rocha <isabele.rocha@bemobi.com>
2025-06-24 20:55:45 -0300

    gera txt para dijkstra

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 from graph import Graph
+
 def main():
     g = Graph(5, representation="adjacency_list")
     g.read_graph("input.txt")
 
-    # informacoes gerais
     g.write_graph_info("info.txt")
     print("Arquivo info.txt gerado com informações do grafo.")
 
-
-    # busca em largura (BFS)
     pais_bfs, niveis_bfs = g.bfs(start=1)
     g.write_search_tree(pais_bfs, niveis_bfs, "bfs.txt")
+    print("Arquivo bfs.txt gerado com os resultados da BFS.")
 
-    # busca em profundidade (DFS)
     pais_dfs = g.dfs(start=1)
-    g.write_search_tree(pais_dfs, {v: -1 for v in pais_dfs}, "dfs.txt") 
+    g.write_search_tree(pais_dfs, {v: -1 for v in pais_dfs}, "dfs.txt")
+    print("Arquivo dfs.txt gerado com os resultados da DFS.")
 
-    # componentes conexos
     g.write_components("componentes.txt")
+    print("Arquivo componentes.txt gerado com os componentes conexos.")
 
-
- #Caminho mínimo usando Dijkstra
     origem = 1
     destino = 8
-
     caminho, distancia = g.shortest_path(origem, destino)
-    print(f"Caminho mínimo de {origem} até {destino}: {caminho} com distância {distancia}")
-
-    #Distância de um vértice para todos
     distancias, _ = g.dijkstra(origem)
-    print(f"Distâncias do vértice {origem} para todos os outros vértices:")
-    for v, d in distancias.items():
-        print(f"{origem} -> {v}: {d}")
 
+    with open("dijkstra.txt", "w") as f:
+        f.write(f"Caminho mínimo de {origem} até {destino}: {caminho} com distância {distancia}\n\n")
+        f.write(f"Distâncias do vértice {origem} para todos os outros vértices:\n")
+        for v, d in distancias.items():
+            f.write(f"{origem} -> {v}: {d}\n")
+    print("Arquivo dijkstra.txt gerado com os resultados do Dijkstra.")
 
 if __name__ == "__main__":
     main()
